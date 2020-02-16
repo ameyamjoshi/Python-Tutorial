21b7b3093534437b91e40fde7ccd2ede8553c55d
Ameya Joshi <52293882+ameyamjoshi@users.noreply.github.com>
2020-02-16 12:06:27 +0530

    Number datatype in Python

---
+++ numbers.py
@@ -0,0 +1,23 @@
+#print(2+2)
+
+#print(50 - 5*6)
+
+# print(8 / 5)  # division always returns a floating point number
+
+# print(8//5)  # floor division discards the fractional part
+
+#print(5 ** 2)  # 5 squared
+
+#print(2 ** 7)  # 2 to the power of 7
+
+#print(4 * 3.75 - 1)
+
+"""
+In interactive mode, the last printed expression is assigned to the variable _.
+This means that when you are using Python as a desk calculator, it is somewhat easier to continue calculations
+example:
+>>> 2+2
+4
+>>> 3+_
+7
+"""
