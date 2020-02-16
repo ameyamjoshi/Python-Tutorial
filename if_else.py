5ebe1a573df0ba88351a014b18b896c8dccc63f6
Ameya Joshi <52293882+ameyamjoshi@users.noreply.github.com>
2020-02-16 21:40:11 +0530

    if_else in python

---
+++ if_else.py
@@ -0,0 +1,47 @@
+# a = 33
+# b = 200
+# if b > a:
+#   print("b is greater than a") #note the indentation
+
+# a = 33
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:  #elif means else if 
+#   print("a and b are equal")
+
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+# a=20
+# b=3
+# if a > b: print("a is greater than b") #Short hand if statement
+
+# a = 2
+# b = 330
+# print("A") if a > b else print("B") #Short hand if else
+
+
+# a = 330
+# b = 330
+# print("A") if a > b else print("=") if a == b else print("B") #3 conditions
+
+# a = 200
+# b = 33
+# c = 500
+# if a > b and c > a: #logical operators can be used in if statements(and or)
+#   print("Both conditions are True")
+
+# x = 41
+# if x > 10:
+#   print("Above ten,")
+#   if x > 20: #Nested if statements
+#     print("and also above 20!")
+#   else:
+#     print("but not above 20.")
